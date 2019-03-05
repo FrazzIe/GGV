b026f7ac5a946785c3da57c735224525a54f543f
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-05 17:08:21 +0000

    Added keys, setup bodyParser, cookies and passport

--- Website/express-server/app.js
+++ Website/express-server/app.js
@@ -3,7 +3,19 @@ const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const SteamStrategy = require('passport-steam').Strategy;
+const keys = require('./config/keys');
 
 const publicRoot = '../vue-client/dist';
 
 const app = express();
+
+app.use(express.static(publicRoot))
+app.use(bodyParser.json())
+
+app.use(cookieSession({  
+    keys: keys.session.cookieKeys,
+    maxAge: 24 * 60 * 60 * 1000
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
