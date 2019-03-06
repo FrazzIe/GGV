a51938176cbc8015040f032283506bc6a29fb0ba
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-06 23:42:00 +0000

    Added sql statements

---
+++ Website/express-server/config/sql.js
@@ -0,0 +1,9 @@
+module.exports = {
+	getUser: "SELECT UNIX_TIMESTAMP(player.timestamp) As 'timestamp', UNIX_TIMESTAMP(player.lastplayed) As 'lastplayed', statistic.wins, statistic.games_played, statistic.kills, statistic.deaths FROM player JOIN statistic ON player.id = statistic.player_id WHERE player.steam = ?",
+	isBanned: "SELECT ban.reason, ban.expire FROM ban JOIN player ON ban.player_id = player.id WHERE player.steam = ?",
+	topKills: "SELECT statistic.kills, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.kills LIMIT 10",
+	topDeaths: "SELECT statistic.deaths, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.deaths LIMIT 10",
+	topWins: "SELECT statistic.wins, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.wins LIMIT 10",
+	topLosses: "SELECT statistic.games_played - statistic.wins As 'losses', player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY 'losses' LIMIT 10",
+	}
+}
