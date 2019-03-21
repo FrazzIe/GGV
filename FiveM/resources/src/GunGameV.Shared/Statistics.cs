01e504640fc5548ee8df6338da9178c632e4ccce
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 17:19:52 +0000

    Upload of Shared Statistics classes

---
+++ FiveM/resources/src/GunGameV.Shared/Statistics.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunGameV.Shared
+{
+    abstract public class Statistics
+    {
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+    }
+
+    public class GlobalStats : Statistics
+    {
+        private int gamesPlayed;
+
+        public GlobalStats(int kills = 0, int deaths = 0, int wins = 0, int _gamesPlayed = 0)
+        {
+            Kills = kills;
+            Deaths = deaths;
+            Wins = wins;
+            gamesPlayed = _gamesPlayed;
+        }
+        public int Wins { get; set; }
+        public int Loses { get => gamesPlayed - Wins; }
+    }
+
+    public class GameStats : Statistics
+    {
+        public GameStats(int kills = 0, int deaths = 0, int score = 0)
+        {
+            Kills = kills;
+            Deaths = deaths;
+            Score = score;
+        }
+        public int Score { get; set; }
+    }
+}
