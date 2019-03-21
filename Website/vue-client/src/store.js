5da87ecccceca33868824e2722f9d17d1959387f
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 16:25:21 +0000

    Added navgation items and user ref

--- Website/vue-client/src/store.js
+++ Website/vue-client/src/store.js
@@ -5,10 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+  	navigation: [
+  		{icon: "home", title: "Home", route: "/"},
+  		{icon: "info", title: "About", route: "/about"},
+  	],
+  	userNavigation: [
+  		{icon: "account_box", title: "Dashboard", route: "/dashboard"},
+  	],
+  	user: false
   },
   mutations: {
-
+  	setUser(_, userState) {
+  		this.state.user = userState;
+  	}
   },
   actions: {
 
