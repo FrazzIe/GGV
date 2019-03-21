950e6f580a02ca1b6b76b0c39bd1f2214359a11a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 16:28:10 +0000

    Setup of router views

--- Website/vue-client/src/router.js
+++ Website/vue-client/src/router.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Dashboard from './views/Dashboard.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  //mode: 'history',
+  //base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
@@ -16,10 +19,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: About
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    },
   ]
 })
