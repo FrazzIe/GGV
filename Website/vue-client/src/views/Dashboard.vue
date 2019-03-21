e806207677ab86dc8a2a92062c1ea90fc0828697
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 16:28:46 +0000

    User dashbored base

---
+++ Website/vue-client/src/views/Dashboard.vue
@@ -0,0 +1,56 @@
+<template>
+	<v-container grid-list-md text-xs-center>
+		<span class="title">Welcome Frazzle.!</span>
+		<v-layout align-center justify-center row fill-height>
+			<v-flex xs3>
+				<img src="../assets/logo.png">
+			</v-flex>
+			<v-flex xs3>
+				<img src="../assets/logo.png">
+			</v-flex>
+			<v-flex xs3>
+				<img src="../assets/logo.png">
+			</v-flex>
+			<v-flex xs3>
+				<img src="../assets/logo.png">
+			</v-flex>
+		</v-layout>
+		<v-layout align-center justify-center row fill-height>
+			<v-flex xs3>
+				<span class="caption">50</span>
+			</v-flex>
+			<v-flex xs3>
+				<span class="caption">50</span>
+			</v-flex>
+			<v-flex xs3>
+				<span class="caption">50</span>
+			</v-flex>
+			<v-flex xs3>
+				<span class="caption">50</span>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+  	...mapState(['user'])
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+<style>
+
+</style>
