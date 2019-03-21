6efc7611efbe66de68bf21b935913ab1c4ed9144
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 15:48:35 +0000

    Add get request to handle login and return after a log in attempt

--- Website/express-server/app.js
+++ Website/express-server/app.js
@@ -24,3 +24,35 @@ app.use(passport.session());
 app.get("/", (req, res, next) => {
   res.sendFile("index.html", { root: publicRoot})
 })
+
+app.get("/login", (req, res, next) => {  
+  passport.authenticate("steam", (err, profile, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!profile) {
+      return res.status(400).send([profile, "Cannot log in", info]);
+    }
+
+    req.login(profile, err => {
+      res.send("Logged in");
+    });
+  })(req, res, next);
+});
+
+app.get("/return", (req, res, next) => {  
+  passport.authenticate("steam", (err, profile, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!profile) {
+      return res.status(400).send([profile, "Cannot log in", info]);
+    }
+
+    req.login(profile, err => {
+      res.redirect("/");
+    });
+  })(req, res, next);
+});
