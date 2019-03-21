aa5196046a335487d5458f7d23eca800c32ea5f5
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 15:44:10 +0000

    Added mysql connection

--- Website/express-server/config/sql.js
+++ Website/express-server/config/sql.js
@@ -1,3 +1,10 @@
+const mysql = require('mysql');
+const keys = require('./keys');
+
+var connection = mysql.createConnection(keys.mysql);
+
+connection.connect()
+
 module.exports = {
 	getUser: "SELECT UNIX_TIMESTAMP(player.timestamp) As 'timestamp', UNIX_TIMESTAMP(player.lastplayed) As 'lastplayed', statistic.wins, statistic.games_played, statistic.kills, statistic.deaths FROM player JOIN statistic ON player.id = statistic.player_id WHERE player.steam = ?",
 	isBanned: "SELECT ban.reason, ban.expire FROM ban JOIN player ON ban.player_id = player.id WHERE player.steam = ?",
@@ -5,7 +12,7 @@ module.exports = {
 	topDeaths: "SELECT statistic.deaths, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.deaths LIMIT 10",
 	topWins: "SELECT statistic.wins, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.wins LIMIT 10",
 	topLosses: "SELECT statistic.games_played - statistic.wins As 'losses', player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY 'losses' LIMIT 10",
-	execute: function(sql, params, connection) {
+	execute: function(sql, params) {
 	  return new Promise((resolve, reject) => {
 	    connection.query(sql, params, (error, result, fields) => {
 	      if (error) reject(error);
