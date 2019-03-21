7ce7cce82393728da11f74210a9463ac348eb766
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 17:18:42 +0000

    Uploa of Client and Server class files

---
+++ FiveM/resources/src/GunGameV.Client/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core;
+
+namespace GunGameV.Client
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+
+        }
+    }
+}
---
+++ FiveM/resources/src/GunGameV.Server/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core;
+
+namespace GunGameV.Server
+{
+    public class Server : BaseScript
+    {
+        public Server()
+        {
+            EventHandlers["playerConnecting"] += new Action<Player, string, dynamic, dynamic>(OnPlayerConnecting);
+        }
+
+        private void OnPlayerConnecting([FromSource]Player player, string playerName, dynamic setKickReason, dynamic deferrals)
+        {
+            deferrals.defer();
+        }
+    }
+}
