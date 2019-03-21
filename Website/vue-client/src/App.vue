30a6e45b924772fe1508abbe1d204b9b7e6d7b5a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 16:27:45 +0000

    Avatar user panel with a logout button

--- Website/vue-client/src/App.vue
+++ Website/vue-client/src/App.vue
@@ -30,6 +30,59 @@
       <a href="/login" v-if="!user">
         <img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png">
       </a>
+
+      <v-menu v-model="menu" :close-on-content-click="false" :nudge-bottom="40" left v-if="user">
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-if="user" v-on="on">
+            <v-avatar size="36px">
+              <img :src="user.avatar">
+            </v-avatar>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-avatar>
+                <img :src="user.avatar">
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title>{{ user.name }}</v-list-tile-title>
+                <v-list-tile-sub-title><a :href="user.link">{{ user.steam }}</a></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-tile v-for="page in userNavigation" :key="page.title" :to="page.route">
+              <v-list-tile-action>
+                <v-icon>{{ page.icon }}</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ page.title }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile href="/logout">
+              <v-list-tile-action>
+                <v-icon>exit_to_app</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  Logout
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-menu>
     </v-toolbar>
 
     <v-content>
@@ -50,6 +103,7 @@ export default {
   data () {
     return {
       drawer: true,
+      menu: false
     }
   },
   computed: {
