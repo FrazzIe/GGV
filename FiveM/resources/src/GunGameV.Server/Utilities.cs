56dacf2975b73950959858204fea5b52fed9b732
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 17:19:16 +0000

    Upload of Server Utilities class

---
+++ FiveM/resources/src/GunGameV.Server/Utilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace GunGameV.Server
+{
+    public static class Utilities
+    {
+        public static string GetLicense(Player player) {
+            return player.Identifiers["license"];
+        }
+
+        public static string GetSteam(Player player)
+        {
+            return player.Identifiers["steam"];
+        }
+
+        public static string GetSteam64(Player player)
+        {
+            string steam = GetSteam(player);
+
+            if (steam != null)
+            {
+                return Convert.ToInt32(steam, 16).ToString();
+            } else
+            {
+                return null;
+            }
+        }
+        public static string GetIP(Player player)
+        {
+            return player.Identifiers["ip"];
+        }
+        public static long GetUnixTimestamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        }
+    }
+}
