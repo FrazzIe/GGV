5d60b2b63433c8972d31d49dc0c627bb1f839889
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 17:20:06 +0000

    Upload of Shared User class

---
+++ FiveM/resources/src/GunGameV.Shared/User.cs
@@ -0,0 +1,26 @@
+﻿namespace GunGameV.Shared
+{
+    public class User
+    {
+        private int id;
+        private string name, steam, license, ip;
+
+        public GlobalStats globalStats;
+        public GameStats gameStats = new GameStats();
+
+        public User(int _id, string _name, string _steam, string _license, string _ip, int kills = 0, int deaths = 0, int wins = 0, int gamesPlayed = 0)
+        {
+            id = _id;
+            name = _name;
+            steam = _steam;
+            license = _license;
+            ip = _ip;
+            globalStats = new GlobalStats(kills, deaths, wins, gamesPlayed);
+        }
+        public int ID { get => id; }
+        public string Name { get => name; }
+        public string Steam { get => steam; }
+        public string License { get => license; }
+        public string IP { get => ip; }
+    }
+}
