e307adc98dade9dee3e5e50f37d6abb7b7a8df94
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-21 17:34:18 +0000

    Create resource file for resource, add resource to server cfg

---
+++ FiveM/resources/ggv/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
+
+shared_scripts {
+	"GunGameV.Shared.net.dll",
+}
+client_scripts {
+	"GunGameV.Client.net.dll",
+}
+
+server_scripts {
+	"GunGameV.Server.net.dll",
+}
