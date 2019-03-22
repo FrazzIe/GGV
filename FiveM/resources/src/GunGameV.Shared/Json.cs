866e090f9f76e9deb6bffce66966363d2c0dae3e
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-22 01:10:00 +0000

    Created a shared Json converter class

---
+++ FiveM/resources/src/GunGameV.Shared/Json.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GunGameV.Shared
+{
+    public static class Json
+    {
+        public static string To(object data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        public static object From(string json)
+        {
+            return JObject.Parse(json);
+        }
+    }
+}
