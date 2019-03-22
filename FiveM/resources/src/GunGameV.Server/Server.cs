6207e74a6c2af70e320b829dc030b1f06f74efa6
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-22 01:14:07 +0000

    Added list to hold users, Created event that will add users to the list

--- FiveM/resources/src/GunGameV.Server/Server.cs
+++ FiveM/resources/src/GunGameV.Server/Server.cs
@@ -5,14 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using static CitizenFX.Core.Native.API;
 using CitizenFX.Core;
+using GunGameV.Shared;
 
 namespace GunGameV.Server
 {
     public class Server : BaseScript
     {
+        private List<User> users = new List<User>();
+
         public Server()
         {
+            Debug.WriteLine("SERVER STARTED");
         }
+
+        public List<User> Users { get => users; }
 
         [EventHandler("playerConnecting")]
         private void OnPlayerConnecting([FromSource]Player player, string name, dynamic setKickReason, dynamic deferrals)
@@ -23,6 +29,17 @@ namespace GunGameV.Server
 
             deferrals.done("Get lost!");
         }
+
+        [EventHandler("GGV.Setup")]
+        private void SetupUser([FromSource]Player player)
+        {
+            string steam = Utilities.GetSteam64(player);
+            string license = player.Identifiers["license"];
+            string ip = player.EndPoint;
+
+            Users.Add(new User(int.Parse(player.Handle), player.Name, steam, license, ip));
+
+            TriggerClientEvent("GGV.Sync", -1, Json.To(users));
         }
     }
 }
