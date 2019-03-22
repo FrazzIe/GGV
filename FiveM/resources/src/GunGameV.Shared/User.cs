5a52c2cd4669b38078da639687aae966da2f3a4a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-22 01:09:09 +0000

    Added includes to User.cs

--- FiveM/resources/src/GunGameV.Shared/User.cs
+++ FiveM/resources/src/GunGameV.Shared/User.cs
@@ -1,4 +1,10 @@
-﻿namespace GunGameV.Shared
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunGameV.Shared
 {
     public class User
     {
