c82549a9f3308afc58f2994e9865edbec4d976a8
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-24 23:21:06 +0000

    Added mysql test

--- FiveM/resources/src/GunGameV.Server/Server.cs
+++ FiveM/resources/src/GunGameV.Server/Server.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static CitizenFX.Core.Native.API;
 using CitizenFX.Core;
+using Newtonsoft.Json;
 using GunGameV.Shared;
 
 namespace GunGameV.Server
@@ -12,7 +13,7 @@ namespace GunGameV.Server
     public class Server : BaseScript
     {
         private List<User> users = new List<User>();
-
+        
         public Server()
         {
             Debug.WriteLine("SERVER STARTED");
@@ -26,20 +27,27 @@ namespace GunGameV.Server
             deferrals.defer();
 
             deferrals.update("You have been haulted!");
-
-            deferrals.done("Get lost!");
+            Exports["jssql"].execute("SELECT * FROM player WHERE steam = ?", new[] { "523" }, new Action<dynamic>((result) => 
+            {
+                Debug.WriteLine(result.GetType().FullName);
+                Debug.WriteLine(result[0].GetType().FullName);
+                Debug.WriteLine(result[0].steam);
+            }));
+            deferrals.done("");
         }
 
         [EventHandler("GGV.Setup")]
         private void SetupUser([FromSource]Player player)
         {
+            Debug.WriteLine("Attempting to setup {0}", player.Name);
+
             string steam = Utilities.GetSteam64(player);
             string license = player.Identifiers["license"];
             string ip = player.EndPoint;
 
-            Users.Add(new User(int.Parse(player.Handle), player.Name, steam, license, ip));
+            Users.Add(new User(player.Handle, player.Name, steam, license, ip));
 
-            TriggerClientEvent("GGV.Sync", -1, Json.To(users));
+            TriggerClientEvent("GGV.Sync", JsonConvert.SerializeObject(users));
         }
     }
 }
