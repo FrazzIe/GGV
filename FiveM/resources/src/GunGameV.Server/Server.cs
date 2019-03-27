2a61e68aed24c5b46283b9df44f49abff2640faa
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-03-27 03:30:37 +0000

    Added user database checks, match syncing

--- FiveM/resources/src/GunGameV.Server/Server.cs
+++ FiveM/resources/src/GunGameV.Server/Server.cs
@@ -41,13 +41,35 @@ namespace GunGameV.Server
         {
             Debug.WriteLine("Attempting to setup {0}", player.Name);
 
-            string steam = Utilities.GetSteam64(player);
-            string license = player.Identifiers["license"];
-            string ip = player.EndPoint;
+            string steam64 = Utilities.GetSteam64(player.Identifiers["steam"]);
 
-            Users.Add(new User(player.Handle, player.Name, steam, license, ip));
+            Exports["jssql"].execute("SELECT wins, games_played, kills, deaths FROM player WHERE steam = ?", new[] { "steam64" }, new Action<dynamic>((result) =>
+            {
+                if (result.Count == 0)
+                {
+                    Exports["jssql"].execute("INSERT INTO player (`steam`, `license`, `ip`, `name`) VALUES (?, ?, ?, ?)", new[] { steam64, player.Identifiers["license"], player.EndPoint, player.Name });
 
-            TriggerClientEvent("GGV.Sync", JsonConvert.SerializeObject(users));
+                    Users.Add(new User(player.Handle, player.Name, steam64, player.Identifiers["license"], player.EndPoint));
+
+                    TriggerClientEvent("GGV.Sync.Users", JsonConvert.SerializeObject(users));
+                }
+                else
+                {
+                    Users.Add(new User(player.Handle, player.Name, steam64, player.Identifiers["license"], player.EndPoint, result[0].kills, result[0].deaths, result[0].wins, result[0].games_played));
+
+                    Exports["jssql"].execute("UPDATE player SET ip=?, name=? WHERE steam=?", new[] { player.EndPoint, player.Name, steam64 });
+
+                    TriggerClientEvent("GGV.Sync.Users", JsonConvert.SerializeObject(users));
+                }
+            }));
+
+            if (currentMatch != null)
+            {
+                TriggerClientEvent("GGV.Sync.Match", JsonConvert.SerializeObject(currentMatch));
+            }
+        }
+
+
         }
     }
 }
