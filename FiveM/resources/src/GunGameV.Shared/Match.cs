1478091bae25866d10da65bbf95e2ae1acfe9876
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-05 00:35:16 +0100

    Added a mapName var to Match class

--- FiveM/resources/src/GunGameV.Shared/Match.cs
+++ FiveM/resources/src/GunGameV.Shared/Match.cs
@@ -15,6 +15,8 @@ namespace GunGameV.Shared
         protected int length, scoreLimit;
         [JsonProperty]
         protected List<uint> weapons;
+        [JsonProperty]
+        protected string mapName;
 
         public uint GetWeapon(int index)
         {
@@ -23,6 +25,7 @@ namespace GunGameV.Shared
         public int ScoreLimit { get => scoreLimit; }
         public long StartTime { get => startTime; }
         public long EndTime { get => (startTime + length); }
+        public string Map { get => mapName; }
         public User Winner { get; set; }
     }
 }
