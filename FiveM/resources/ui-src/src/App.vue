879ac3e92676c3cb4f335f5654955b98a18f4cc3
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-07 03:27:05 +0100

    Created a component for the scoreboardd

--- FiveM/resources/ui-src/src/App.vue
+++ FiveM/resources/ui-src/src/App.vue
@@ -7,9 +7,12 @@
 </template>
 
 <script>
+import Scoreboard from './components/Scoreboard'
+
 export default {
   name: 'App',
   components: {
+    Scoreboard
   },
   data () {
     return {
---
+++ FiveM/resources/ui-src/src/components/Scoreboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fill-height v-if="visible">
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+  }
+</script>
+
+<style>
+  ::-webkit-scrollbar { 
+      display: none; 
+  }
+</style>
