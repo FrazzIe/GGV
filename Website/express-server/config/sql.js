8e0e7bca75346a792213c0827560fdbaa884143a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-09 13:04:02 +0100

    Updated outdated sql queries

--- Website/express-server/config/sql.js
+++ Website/express-server/config/sql.js
@@ -6,12 +6,12 @@ var connection = mysql.createConnection(keys.mysql);
 connection.connect()
 
 module.exports = {
-	getUser: "SELECT UNIX_TIMESTAMP(player.timestamp) As 'timestamp', UNIX_TIMESTAMP(player.lastplayed) As 'lastplayed', statistic.wins, statistic.games_played, statistic.kills, statistic.deaths FROM player JOIN statistic ON player.id = statistic.player_id WHERE player.steam = ?",
-	isBanned: "SELECT ban.reason, ban.expire FROM ban JOIN player ON ban.player_id = player.id WHERE player.steam = ?",
-	topKills: "SELECT statistic.kills, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.kills LIMIT 10",
-	topDeaths: "SELECT statistic.deaths, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.deaths LIMIT 10",
-	topWins: "SELECT statistic.wins, player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY statistic.wins LIMIT 10",
-	topLosses: "SELECT statistic.games_played - statistic.wins As 'losses', player.name, player.steam FROM statistic JOIN player ON statistic.player_id = player.id ORDER BY 'losses' LIMIT 10",
+	getUser: "SELECT UNIX_TIMESTAMP(timestamp) As 'timestamp', UNIX_TIMESTAMP(lastplayed) As 'lastplayed', wins, games_played, kills, deaths FROM player WHERE steam = ?",
+	isBanned: "SELECT ban.reason, ban.expire FROM ban JOIN player ON ban.player_id = id WHERE steam = ?",
+	topKills: "SELECT kills, name, steam FROM player ORDER BY kills LIMIT 10",
+	topDeaths: "SELECT deaths, name, steam FROM player ORDER BY deaths LIMIT 10",
+	topWins: "SELECT wins, name, steam FROM player ORDER BY wins LIMIT 10",
+	topLosses: "SELECT games_played - wins As 'losses', name, steam FROM player ORDER BY 'losses' LIMIT 10",
 	execute: function(sql, params) {
 	  return new Promise((resolve, reject) => {
 	    connection.query(sql, params, (error, result, fields) => {
