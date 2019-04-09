7b162ca4a3ba500347c1915f4a0fc032e872b93a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-09 13:06:55 +0100

    Created a user stats page with icons

---
+++ Website/vue-client/src/components/Account.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container fill-height grid-list-md v-if="user">
+    <v-layout text-xs-center align-center justify-center>
+      <v-flex xs3>
+        <v-layout column>
+          <v-flex xs>
+            Kills
+          </v-flex xs>
+          <v-flex xs>
+            <img src="../assets/rifle.svg" width="200" height="200" alt="weapon image">
+          </v-flex>
+          <v-flex xs>
+            <span class="subheading">{{ user.kills }}</span>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs3>
+        <v-layout column>
+          <v-flex xs>
+            Deaths
+          </v-flex xs>
+          <v-flex xs>
+            <img src="../assets/skull.svg" width="200" height="200" alt="skull image">
+          </v-flex>
+          <v-flex xs>
+            <span class="subheading">{{ user.deaths }}</span>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs3>
+        <v-layout column>
+          <v-flex xs>
+            Wins
+          </v-flex xs>
+          <v-flex xs>
+            <img src="../assets/winner.svg" width="200" height="200" alt="star image">
+          </v-flex>
+          <v-flex xs>
+            <span class="subheading">{{ user.wins }}</span>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs3>
+        <v-layout column>
+          <v-flex xs>
+            Losses
+          </v-flex xs>
+          <v-flex xs>
+            <img src="../assets/stairs.svg" width="200" height="200" alt="lose image">
+          </v-flex>
+          <v-flex xs>
+            <span class="subheading">{{ user.games_played - user.wins }}</span>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import { mapState, mapMutations } from 'vuex';
+
+  export default {
+    data () {
+      return {
+      }
+    },
+    computed: {
+      ...mapState(['user'])
+    },
+  }
+</script>
+
+<style>
+
+</style>
