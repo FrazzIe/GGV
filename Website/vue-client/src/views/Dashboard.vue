496b343a2731eae19abdc88d8f3f62f4af76b176
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-09 13:06:01 +0100

    Removed garabge from View file

--- Website/vue-client/src/views/Dashboard.vue
+++ Website/vue-client/src/views/Dashboard.vue
@@ -1,56 +1,13 @@
 <template>
-	<v-container grid-list-md text-xs-center>
-		<span class="title">Welcome Frazzle.!</span>
-		<v-layout align-center justify-center row fill-height>
-			<v-flex xs3>
-				<img src="../assets/logo.png">
-			</v-flex>
-			<v-flex xs3>
-				<img src="../assets/logo.png">
-			</v-flex>
-			<v-flex xs3>
-				<img src="../assets/logo.png">
-			</v-flex>
-			<v-flex xs3>
-				<img src="../assets/logo.png">
-			</v-flex>
-		</v-layout>
-		<v-layout align-center justify-center row fill-height>
-			<v-flex xs3>
-				<span class="caption">50</span>
-			</v-flex>
-			<v-flex xs3>
-				<span class="caption">50</span>
-			</v-flex>
-			<v-flex xs3>
-				<span class="caption">50</span>
-			</v-flex>
-			<v-flex xs3>
-				<span class="caption">50</span>
-			</v-flex>
-		</v-layout>
-	</v-container>
+  <Account/>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
+  import Account from '../components/Account'
 
-export default {
-  data () {
-    return {
-
+  export default {
+    components: {
+      Account
     }
-  },
-  computed: {
-  	...mapState(['user'])
-  },
-  methods: {
-
   }
-}
-
 </script>
-
-<style>
-
-</style>
