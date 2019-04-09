b05b2bec25d72654bbc11b84e10610e7bb691f2a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-09 20:07:05 +0100

    Added some info to the about page

---
+++ Website/vue-client/src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fill-height grid-list-md>
+    <v-layout text-xs-center align-center justify-center wrap>
+      <v-flex xs12>
+        <p>This website is a way to view and track Statistics for Gun Game V without needing to be in game.</p>
+        <p>The site has very basic functions, such as searching for a specific player, logging in to view your own statistics and viewing the top 10 players for each category.</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
--- Website/vue-client/src/views/About.vue
+++ Website/vue-client/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <About/>
 </template>
+
+<script>
+  import About from '../components/About'
+
+  export default {
+    components: {
+      About
+    }
+  }
+</script>
